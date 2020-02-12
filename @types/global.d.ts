b7baba4085774f2bff5b92af44f57e769f7d7563
Oskar Rainer <rainer.oskar@gmail.com>
2020-02-12 15:03:49 +0100

    add gsap and plugins

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,3 +1,13 @@
+declare global {
+  import { TweenMax as ITweenMax } from 'gsap'
+  import { Draggable as IDraggable } from 'gsap/Draggable'
+  import { InertiaPlugin as IInertiaPlugin } from 'gsap/InertiaPlugin'
+
+  declare var TweenMax: ITweenMax
+  declare var Draggable: IDraggable
+  declare var InertiaPlugin: IInertiaPlugin
+}
+
 declare module '!!raw-loader*' {
   const payload: string
   export default payload
